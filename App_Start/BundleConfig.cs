7062819f57bb2802c9ebcc384e582c891a7e6cd7
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-10-31 10:38:08 +0700

    Add Bootstrap 4.1.x, change namespace, solution and project names

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace FMSWeb
+namespace FDMSWeb
 {
     public class BundleConfig
     {
--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace FMSWeb
+namespace FDMSWeb
 {
     public class FilterConfig
     {
