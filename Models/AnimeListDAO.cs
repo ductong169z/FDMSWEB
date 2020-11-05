5f19ca3696ffe89f0052e99528be58bd912c12a3
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-11-05 08:14:53 +0700

    Comment for DAO

--- Models/AnimeListDAO.cs
+++ Models/AnimeListDAO.cs
@@ -6,35 +6,47 @@ using System.Web;
 
 namespace FDMSWeb.Models
 {
+    /* DAO for FDMS Anime List Website */
     public class AnimeListDAO
     {
+        /* Constructor */
         public AnimeListDAO()
         {
         }
 
+        /// <summary>
+        /// Get all seasons from database
+        /// </summary>
+        /// <returns>A list of all seasons available</returns>
         public List<Season> GetSeasons()
         {
-            MySqlConnection conn = null;
-            MySqlCommand cmd;
-            MySqlDataReader rd = null;
-            List<Season> seasonList = null;
+            /* Declare resources used for interacting with database */
+            MySqlConnection conn = null; // connnection to database
+            MySqlCommand cmd; // store SQL statement
+            MySqlDataReader rd = null; // reader for return results
+            List<Season> seasonList = null; // list of all seasons
             try
             {
-                conn = DBUtils.GetConnection();
-                conn.Open();
-                cmd = new MySqlCommand("SELECT * FROM season WHERE deleted_at IS NULL", conn);
-                rd = cmd.ExecuteReader();
+                conn = DBUtils.GetConnection(); // get connection to database
+                conn.Open(); // open the connection
+                cmd = new MySqlCommand("SELECT * FROM season WHERE deleted_at IS NULL", conn); // SQL statement
+                rd = cmd.ExecuteReader(); // execute the SQL statement and store results to reader
 
+                /* Keep reading and adding data to list until end */
                 while (rd.Read())
                 {
+                    /* Temp vars to store Season properties */
                     int id = rd.GetInt32(0);
                     String name = rd.GetString(1);
                     DateTime created_at = rd.GetDateTime(2);
 
+                    // instantiate if list has not yet been instantiated
                     if (seasonList == null)
                     {
                         seasonList = new List<Season>();
                     }
+
+                    // add new Season to list
                     seasonList.Add(new Season(id, name, created_at));
                 }
 
@@ -46,6 +58,7 @@ namespace FDMSWeb.Models
             }
             finally
             {
+                /* Close resources after use */
                 if (conn != null)
                 {
                     conn.Close();
