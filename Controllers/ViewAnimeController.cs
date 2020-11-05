2d21ec558f84ec32402f16b1230d6d3ca8ea35d2
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-11-05 15:25:41 +0700

    Partial view anime controller

---
+++ Controllers/ViewAnimeController.cs
@@ -0,0 +1,31 @@
+﻿using FDMSWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FDMSWeb.Controllers
+{
+    public class ViewAnimeController : Controller
+    {
+        public ActionResult ViewAnime(int id)
+        {
+            /* Instantiate DAO obj and interact with DB */
+            AnimeListDAO dao = new AnimeListDAO();
+            Anime anime = dao.GetAnime(id);
+
+            ///* Check if user logged in to get user anime list */
+
+
+            //List animeInList = null; // not null if there is anime in list
+
+            //if (anime != null)
+            //{
+
+            //}
+
+            return View(anime);
+        }
+    }
+}
