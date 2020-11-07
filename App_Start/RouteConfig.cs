910f9d4075e184faed5b38ae62ac00c48a7f6b61
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-11-07 10:26:26 +0700

    1

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -15,21 +15,21 @@ namespace FDMSWeb
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}",
+                url: "{action}",
                 defaults: new { controller = "Home", action = "Index" }
             );
 
-            routes.MapRoute(
-                name: "View Anime",
-                url: "{action}/animeId={id}",
-                defaults: new { controller = "Anime", action = "ViewAnime", id = 2 }
-            );
+            //routes.MapRoute(
+            //    name: "View Anime",
+            //    url: "{action}/animeId={id}",
+            //    defaults: new { controller = "Anime", action = "ViewAnime", id = 2 }
+            //);
 
-            routes.MapRoute(
-                name: "View Anime List",
-                url: "{action}/accountId={accountId}&listStatus={listStatus}",
-                defaults: new { controller = "Anime", action = "ViewAnimeList", id = 3 }
-            );
+            //routes.MapRoute(
+            //    name: "View Anime List",
+            //    url: "{action}/accountId={accountId}&listStatus={listStatus}",
+            //    defaults: new { controller = "Anime", action = "ViewAnimeList", id = 3 }
+            //);
         }
     }
 }
