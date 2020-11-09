1c08702cd97774038d8fffc52e06683ed4235c8b
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-11-09 20:30:51 +0700

    Comments for some views

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -3,27 +3,29 @@ using System.Web.Optimization;
 
 namespace FDMSWeb
 {
+    /* Bundel Configuration */
     public class BundleConfig
     {
+        /// <summary>
+        /// Register bundles for the project to use
+        /// </summary>
+        /// <param name="bundles"></param>
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Library/Bootstrap/js/jquery-3.5.1.slim.min.js"));
+                        "~/Library/Bootstrap/js/jquery-3.5.1.slim.min.js")); // add jquery bundle
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*")); // add jquery validate bundle
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*")); // add mondernizr bundle
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Library/Bootstrap/js/bootstrap.js"));
+                      "~/Library/Bootstrap/js/bootstrap.js")); // add bootstrap javascript bundle
 
             bundles.Add(new StyleBundle("~/bundles/css").Include(
-                      "~/Library/Bootstrap/css/bootstrap.css"));
-
-            bundles.Add(new ScriptBundle("~/Library/fontawesome").Include(
-                      "~/Library/fontawesome.js"));
+                      "~/Library/Bootstrap/css/bootstrap.css")); // add bootstrap css bundle
         }
     }
 }
--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -3,11 +3,16 @@ using System.Web.Mvc;
 
 namespace FDMSWeb
 {
+    /* Filter Configuration*/
     public class FilterConfig
     {
+        /// <summary>
+        /// Register global filters for the project to use
+        /// </summary>
+        /// <param name="filters"></param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute()); // add default Handle Error Attribute to handle errors
         }
     }
 }
