ff338367132cad85e116781738feab3f7f7e74d9
Nagidrop <58417614+Nagidrop@users.noreply.github.com>
2020-11-10 17:34:18 +0700

    Add comments for Controller classes and DAO methods

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,34 +7,34 @@ using System.Web.Mvc;
 
 namespace FDMSWeb.Controllers
 {
+    /// <summary>
+    /// Controller to show index page
+    /// </summary>
     public class HomeController : Controller
     {
       
+        /// <summary>
+        /// Show index page
+        /// </summary>
+        /// <returns>Index page or log in page</returns>
         public ActionResult Index()
         {
-            AnimeListDAO dao = new AnimeListDAO();
-            List<Anime> listAnime = dao.GetAnimes(12);
-            ViewBag.listAnime = listAnime; List<Season> seasonList = dao.GetAllSeasons();
+            /* Check if user logged in */
             if (Session["User"] == null)
             {
                 return RedirectToAction("Login", "Authentication");
             }
             else
             {
-                return View(seasonList);
+                /* Instantiate DAO obj and interact with DB */
+                AnimeListDAO dao = new AnimeListDAO();
+                List<Anime> listAnime = dao.GetAnimes(12);
+
+                /* Set value to ViewBag to display */
+                ViewBag.listAnime = listAnime;
+
+                return View();
             }
-        }
-
-        public ActionResult ViewAnime()
-        {
-
-            return View("~/Views/Anime/ViewAnime.cshtml");
-        }
-
-        public ActionResult ViewAnimeList()
-        {
-
-            return View("~/Views/Anime/ViewAnimeList.cshtml");
         }
     }
 }
